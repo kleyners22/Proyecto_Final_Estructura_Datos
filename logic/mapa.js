3f85c207c231fe223d3a54e5a06e96bd7fe3e413
JohanelENP <johanel.castillo@unl.edu.ec>
2026-07-24 21:31:30 -0500

    Delete logic/mapa.js

--- logic/mapa.js
+++
@@ -1,7 +0,0 @@
-// mapa.js — Inicializacion del mapa Leaflet
-
-let mapa = L.map('mapa').setView([-3.9931, -79.2042], 13);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'OpenStreetMap'
-}).addTo(mapa);
